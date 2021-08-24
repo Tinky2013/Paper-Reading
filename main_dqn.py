8bf77e792ab00e4c9007fca4fa03ebd97b97359b
Ziyue Wu <670122025@qq.com>
2021-08-24 21:29:02 +0800

    Add files via upload

---
+++ main_dqn.py
@@ -0,0 +1,127 @@
+
+from stable_baselines3 import DQN
+
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.sac.policies import MlpPolicy
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import BaseCallback
+import os
+import numpy as np
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import seaborn as sns
+from collections import deque
+import random
+import time
+import gym
+import os
+import h5py
+
+from envR import ENVR
+from envS import ENVS
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print("Num timesteps: {}".format(self.num_timesteps))
+                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print("Saving new best model to {}".format(self.save_path))
+                  self.model.save(self.save_path)
+
+        return True
+
+
+def plot_results(log_folder):
+    from scipy.signal import savgol_filter
+    R = load_results(log_folder)['r']
+    T = load_results(log_folder)['t']
+    # _w = 7
+    # _window_size = len(R) // _w if (len(R) // _w) % 2 != 0 else len(R) // _w + 1
+    # filtered = savgol_filter(R, _window_size, 1)
+
+    #plt.title('smoothed returns')
+    plt.title('returns')
+    plt.ylabel('Returns')
+    plt.xlabel('time step')
+    plt.plot(T, R)
+    plt.grid()
+    plt.show()
+
+
+def train_dqn():
+
+    log_dir = f"model_save/"
+    env = ENVR()
+    env = Monitor(env, log_dir)
+    env = DummyVecEnv([lambda: env])
+    # env = VecNormalize(env, norm_obs=True, norm_reward=True,
+    #                clip_obs=10.)
+    model = DQN('MlpPolicy', env, verbose=1)
+    callback = SaveOnBestTrainingRewardCallback(check_freq=100, log_dir=log_dir)
+    model.learn(total_timesteps=int(100000), callback = callback, log_interval = 100)
+    model.save('model_save/dqn')
+
+def test_dqn():
+    log_dir = f"model_save/best_model"
+    env = ENVS()
+    env.render = True
+    env = Monitor(env, log_dir)
+    model = DQN.load(log_dir)
+    plot_results(f"model_save/")
+    state = env.reset()
+    profit = 0
+    i = 0
+    while True:
+        i+=1
+        action = model.predict(state)
+        next_state, reward, done, info = env.step(action[0])
+        profit += env.profit
+        print("trying:",i,"action:", action,"try profit:",env.profit,"total profit:",profit)
+        if done:
+            print('finish! total profit=',profit)
+            break
+
+if __name__ == '__main__':
+    # log_dir = "tmp/"
+    # os.makedirs(log_dir, exist_ok=True)
+    train_dqn()
+    test_dqn()
