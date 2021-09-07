c6c5205a1b56d2907183aea6aafe5ceeed4fc112
Ziyue Wu <670122025@qq.com>
2021-09-07 08:39:08 +0800

    Add files via upload

--- normalize.py
+++ normalize.py
@@ -7,9 +7,11 @@ df = pd.read_csv(r'hd5_sz50/2019_sz50n.csv')
 nor_dt = df.drop(['time','thscode','amount','CLOSE_AFTER'], axis=1)
 id_dt = df[['time','thscode','amount','CLOSE_AFTER']]
 
-nor_dt = nor_dt.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+# nor_dt = nor_dt.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+
+nor_dt = nor_dt.apply(lambda x: (x - np.mean(x)) / (np.std(x)))
 
 data_normalized = pd.concat([id_dt,nor_dt],axis=1)
 
-#data_normalized.to_csv('2019_sz5n.csv',index=False)
-data_normalized.to_csv('hd5_sz50/2019_sz50n.csv',index=False)
+#data_normalized.to_csv('2019_sz5s.csv',index=False)
+data_normalized.to_csv('hd5_sz50/2019_sz50s.csv',index=False)
