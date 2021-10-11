716e456ae3d086e3dcd96623f1f7f1ec415dd378
Ziyue Wu <670122025@qq.com>
2021-10-11 21:27:28 +0800

    Add files via upload

--- visualize.py
+++ visualize.py
@@ -2,37 +2,68 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-MODEL_PATH = 'ppo_sp2'
+MODEL_PATH = 'td3_sp2'
 
 trade_dt = pd.read_csv(r'out_dt/trade_'+MODEL_PATH+'.csv')
 result_dt = pd.read_csv(r'out_dt/result_'+MODEL_PATH+'.csv')
+trade_dayend_dt = pd.read_csv(r'out_dt/trade_'+MODEL_PATH+'_dayend.csv')
+zz500r_dt = pd.read_csv(r'out_dt/zz500_ratio.csv')
 
-price_col = ['stock_price_'+str(i) for i in range(453)]
-port_col = ['stock_port_'+str(i) for i in range(453)]
+bh_col = ['stock_bh_'+str(i) for i in range(10)]
+port_col = ['stock_port_'+str(i) for i in range(10)]
 
-price_mean = trade_dt[price_col].mean(axis=1)
+zz500_ratio = zz500r_dt.loc[(zz500r_dt['zz']>='2019-01-02')&(zz500r_dt['zz']<'2020-01-02')]['zz500ratio']
+bh_mean = trade_dt[bh_col].mean(axis=1)
+for i in range(len(bh_mean)):
+    bh_mean[i]=bh_mean[i]+1
+zz500_total = [1]
+for i in range(len(zz500_ratio)):
+    zz500_total.append((zz500_total[i]*(1+zz500_ratio.iloc[i]/100)))
 port_mean = trade_dt[port_col].mean(axis=1)
+port_dayend_mean = trade_dayend_dt[port_col].mean(axis=1)
 go_over_buy_hold = result_dt['prfit(100%)']-result_dt['buy_hold(100%)']
 
-# 绘制交易中平均股票价格-资产图
-t=[i for i in range(len(price_mean))]
+# 绘制交易中平均股票buyhold-资产图
+t=[i for i in range(len(port_mean))]
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-ax1.plot(t,price_mean,"b-",linewidth=2,label="Stock Price")
-ax1.set_ylabel('Stock Price')
-ax1.set_title('Average stock price and portfolio')
+ax1.plot(t[:-400],bh_mean[:-400],"b-",linewidth=2,label="Buy hold")
+ax1.set_ylim((0.5,2.5))
+ax1.set_ylabel('Buy hold')
+ax1.set_title('Average buy-hold and portfolio')
 ax2 = ax1.twinx()  # this is the important function
-ax2.plot(t,port_mean,"g-",linewidth=2,label="Portfolio")
+ax2.plot(t[:-400],port_mean[:-400],"g-",linewidth=2,label="Portfolio")
 # ax2.set_xlim([0, np.e])
+ax2.set_ylim((0.5,2.5))
 ax2.set_ylabel('Portfolio')
 ax2.set_xlabel('Trading time')
 ax1.legend(loc='upper right')
 ax2.legend(loc='upper left')
 plt.grid()
 #plt.show()
-plt.savefig('img/price_port_'+MODEL_PATH+'.jpg')
+plt.savefig('img/bh_port_'+MODEL_PATH+'.jpg')
 
+# 绘制交易中资产-zz500指数图
+t=[i for i in range(len(zz500_ratio))]
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.plot(t[:-1],zz500_total[:-2],"r-",linewidth=2,label="zz500 ratio")
+ax1.set_ylim((0.5,2.5))
+ax1.set_ylabel('zz500 ratio')
+ax1.set_title('zz500 ratio and portfolio')
+ax2 = ax1.twinx()  # this is the important function
+ax2.plot(t[:-1],port_dayend_mean[:-2],"g-",linewidth=2,label="Portfolio")
+# ax2.set_xlim([0, np.e])
+ax2.set_ylim((0.5,2.5))
+ax2.set_ylabel('Portfolio')
+ax2.set_xlabel('Trading time')
+ax1.legend(loc='upper right')
+ax2.legend(loc='upper left')
+plt.grid()
+#plt.show()
+plt.savefig('img/zzratio_port_'+MODEL_PATH+'.jpg')
 
+# 资产收益与buyhold分布图
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.hist(result_dt['prfit(100%)'], bins=100, facecolor="blue", alpha=0.5, label='profit')
---
+++ zz_ppo.py
@@ -0,0 +1,150 @@
+
+from stable_baselines3 import PPO
+
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.sac.policies import MlpPolicy
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import BaseCallback
+import os
+import numpy as np
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import seaborn as sns
+from collections import deque
+import random
+import time
+
+import gym
+import os
+import h5py
+
+from env.zzenv import ENV
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model_'+MODEL_PATH)
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        # if self.save_path is not None:
+        #     os.makedirs(self.save_path, exist_ok=True)
+        pass
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print("Num timesteps: {}".format(self.num_timesteps))
+                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print("Saving new best model to {}".format(self.save_path))
+                  self.model.save(self.save_path)
+
+        return True
+
+
+def plot_results(log_folder):
+    from scipy.signal import savgol_filter
+    R = load_results(log_folder)['r']
+    T = load_results(log_folder)['t']
+    # _w = 7
+    # _window_size = len(R) // _w if (len(R) // _w) % 2 != 0 else len(R) // _w + 1
+    # filtered = savgol_filter(R, _window_size, 1)
+
+    #plt.title('smoothed returns')
+    plt.title('returns')
+    plt.ylabel('Returns')
+    plt.xlabel('time step')
+    plt.plot(T, R)
+    plt.grid()
+    plt.show()
+
+
+def train_ppo():
+
+    log_dir = f"model_save/"
+    env = ENV(istest=False)
+    env = Monitor(env, log_dir)
+    env = DummyVecEnv([lambda: env])
+    # env = VecNormalize(env, norm_obs=True, norm_reward=True,
+    #                clip_obs=10.)
+
+    model = PPO('MlpPolicy', env, verbose=1, batch_size=2048, seed=10)
+    callback = SaveOnBestTrainingRewardCallback(check_freq=480, log_dir=log_dir)
+    model.learn(total_timesteps=int(4000000), callback = callback, log_interval = 480)
+    model.save('model_save/'+MODEL_PATH)
+
+def test_ppo():
+    log_dir = f"model_save/best_model_"+MODEL_PATH
+    env = ENV(istest=True)
+    env.render = True
+    env = Monitor(env, log_dir)
+    model = PPO.load(log_dir)
+    # plot_results(f"model_save/")
+    trade_dt = pd.DataFrame([])     # trade_dt: 所有股票的交易数据
+    result_dt = pd.DataFrame([])    # result_dt: 所有股票一年测试结果数据
+    for i in range(15):
+        state = env.reset()
+        stock_bh_id = 'stock_bh_'+str(i)
+        stock_port_id = 'stock_port_'+str(i)
+        stock_bh_dt = []
+        stock_port_dt = []
+
+        day = 0
+        while True:
+            action = model.predict(state)
+            next_state, reward, done, info = env.step(action[0])
+            state = next_state
+            # print("trying:",day,"reward:", reward,"now profit:",env.profit)
+            stock_bh_dt.append(env.buy_hold)
+            stock_port_dt.append(env.Portfolio_unit)
+            day+=1
+            if done:
+                print('stock: {}, total profit: {:.2f}%, buy hold: {:.2f}%, sp: {:.4f}, mdd: {:.2f}%, romad: {:.4f}'
+                      .format(i, env.profit*100, env.buy_hold*100, env.sp, env.mdd*100, env.romad))
+                result=pd.DataFrame([[i,env.profit*100,env.buy_hold*100,env.sp,env.mdd*100,env.romad]])
+                break
+
+        trade_dt_stock = pd.DataFrame({stock_bh_id: stock_bh_dt, stock_port_id: stock_port_dt}) # trade_dt_stock: 单支股票的交易数据
+        trade_dt = pd.concat([trade_dt, trade_dt_stock], axis=1)
+        result_dt = pd.concat([result_dt,result],axis=0)
+
+    result_dt.columns = ['stock_id','prfit(100%)','buy_hold(100%)','sp','mdd(100%)','romad']
+    trade_dt.to_csv('out_dt/trade_'+MODEL_PATH+'.csv',index=False)
+    result_dt.to_csv('out_dt/result_'+MODEL_PATH+'.csv',index=False)
+
+MODEL_PATH = 'ppo_400w_2018tr'
+
+if __name__ == '__main__':
+    # log_dir = "tmp/"
+    # os.makedirs(log_dir, exist_ok=True)
+    train_ppo()
+    test_ppo()
